4ad3bbd44cd61a6763fff9d4840919c2bc53a02b
Michael Garde <mgarde@gmail.com>
2013-05-07 18:36:52 -0400

    Setup

---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Yeah, Functions! ===
+Contributors: bldware
+Tags: debugging, performance
+Tested up to: 3.5.1
+Stable tag: 0.1
+Requires at least: 3.0
+
+Short description
+
+== Description ==
+Long Description
---
+++ yeah-functions.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Yeah, Functions!
+Plugin URI: https://github.com/MikeGarde/yeah-functions
+Description: A group of functions I regularly use when working with wordpress
+Version: 0.1
+Author: Mike Garde
+Author URI: https://github.com/MikeGarde
+License: undecided
+*/
+
+class yeah_functions {
+	function __construct() {
+		add_action('admin_menu', array(&$this, 'admin_menu'));
+	}
+	function admin_menu() {
+		add_options_page('Documentation',
+						'Yeah Functions',
+						'manage_options',
+						'yeah_functions',
+						array($this, 'docs_page'));
+	}
+	function docs_page() {
+		require_once('view/settings.php');
+	}
+}
+new yeah_functions;
