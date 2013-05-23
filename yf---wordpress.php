7cb416477cfe83f055516f9cd2ff429ca9e82fe6
Michael Garde <mgarde@gmail.com>
2013-05-23 14:54:57 -0400

    get_category_meta

--- yf---wordpress.php
+++ yf---wordpress.php
@@ -9,3 +9,29 @@ function get_top_parent_category($cat_ID) {
 	}
 	return $cat_ID;
 }
+
+if(!function_exists('get_category_meta')) {
+function get_category_meta($id=null) {
+	global $wpdb;
+
+	if($id == null) {
+		$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+		if(!$term->term_id)
+			return false;
+
+		$id = $term->term_id;
+	}
+
+	if (!is_numeric($id)) {}
+
+	$meta = $wpdb->get_results('SELECT `option_name`, `option_value` FROM `wp_options` WHERE `option_name` LIKE "'.$term->taxonomy.'_'.$term->term_id.'_%"');
+	$cutlen = strlen($term->taxonomy.'_'.$term->term_id.'_');
+
+	foreach($meta as $item) {
+		$name = substr($item->option_name, $cutlen);
+		$return[$name] = $item->option_value;
+	}
+
+	return $return;
+}
+}
